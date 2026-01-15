419f9464953cfd3d8e58cfb8a914fd5ed0da8e7e
atk312 <tkwang312@gmail.com>
2026-01-15 14:04:34 -0500

    Created User Entity.

---
+++ services/auth-service/src/main/java/com/tkwang312/auth/model/User.java
@@ -0,0 +1,34 @@
+package com.tkwang312.auth.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    // generate an id, string and username need getters and setters
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    public void setUsername(String usr){
+        this.username = usr;
+    }
+    public void setPasswordHash(String pwd){
+        this.passwordHash = pwd;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+    public String getPasswordHash(){
+        return this.passwordHash;
+    }
+
+}
