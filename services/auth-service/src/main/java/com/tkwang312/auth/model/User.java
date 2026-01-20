5071a5cd2364357115feeea514f197b9d4d0660e
atk312 <tkwang312@gmail.com>
2026-01-20 11:52:05 -0500

    updated User to implement UserDetails class. Added email field as well.

--- services/auth-service/src/main/java/com/tkwang312/auth/model/User.java
+++ services/auth-service/src/main/java/com/tkwang312/auth/model/User.java
@@ -1,15 +1,26 @@
 package com.tkwang312.auth.model;
 
 import jakarta.persistence.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
 
 @Entity
 @Table(name="users")
-public class User {
+public class User implements UserDetails{
 
     // generate an id, string and username need getters and setters
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
+    private String email;
 
     @Column(nullable = false, unique = true)
     private String username;
@@ -17,21 +28,58 @@ public class User {
     @Column(nullable = false)
     private String passwordHash;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
     public void setUsername(String usr){
         this.username = usr;
     }
     public void setPasswordHash(String pwd){
         this.passwordHash = pwd;
     }
+    public void setEmail(String email){this.email = email;}
 
-    public String getUsername(){
-        return this.username;
-    }
+    @Override
+    public String getUsername(){ return this.username; }
     public String getPasswordHash(){
         return this.passwordHash;
     }
+    public String getEmail(){return this.email;}
 
     public Long getId() {
         return this.id;
     }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
 }
