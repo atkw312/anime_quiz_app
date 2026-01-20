43e138f95881a2de9335900c96444d2de7b41eb4
atk312 <tkwang312@gmail.com>
2026-01-20 16:04:49 -0500

    Edited Authentication and security configurations.

--- services/auth-service/src/main/java/com/tkwang312/auth/config/ApplicationConfig.java
+++ services/auth-service/src/main/java/com/tkwang312/auth/config/ApplicationConfig.java
@@ -4,12 +4,18 @@ import com.tkwang312.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
+    private final PasswordEncoder passwordEncoder;
 
     private final UserRepository repository;
 
@@ -18,4 +24,19 @@ public class ApplicationConfig {
         return username -> repository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+
+    //DTO to find user details
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
+    }
+
 }
