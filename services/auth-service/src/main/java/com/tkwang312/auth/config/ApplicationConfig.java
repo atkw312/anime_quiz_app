47ce69ee057ed2bc91c07bf1f7633382ac72d08b
atk312 <tkwang312@gmail.com>
2026-01-20 15:38:13 -0500

    finished updating JwtAuthenticationFilter.

---
+++ services/auth-service/src/main/java/com/tkwang312/auth/config/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package com.tkwang312.auth.config;
+
+import com.tkwang312.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+    }
+}
--- services/auth-service/src/main/java/com/tkwang312/auth/security/JwtAuthenticationFilter.java
+++ services/auth-service/src/main/java/com/tkwang312/auth/security/JwtAuthenticationFilter.java
@@ -5,7 +5,13 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,12 +20,10 @@ import java.io.IOException;
 
 // Goes through filter on each client request.
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
-
-    public JwtAuthenticationFilter(JwtService jwtService){
-        this.jwtService = jwtService;
-    }
+    private final UserDetailsService userDetailsService;
 
     // chain of responsibility design pattern filterchain. List
     @Override
@@ -31,12 +35,34 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // check jwt token
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
+        final String username;
         if (authHeader == null || !authHeader.startsWith("Bearer ")){
             filterChain.doFilter(request, response);
             return;
         }
         // "Bearer " is len 7
         jwt = authHeader.substring(7);
+        username =  jwtService.extractUsername(jwt);
+        //check if user is authenticated yet
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            //get user details from database
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+            //check if user is valid
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                //create object
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails ,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                //enforce token with details of request
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                //update auth token
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+            //pass onto next filter
+            filterChain.doFilter(request, response);
+        }
 
     }
 }
