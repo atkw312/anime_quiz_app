22765d208c95d54dd3dfcbdcbc5d6bd5b8529f57
atk312 <tkwang312@gmail.com>
2026-01-20 12:02:26 -0500

    check Jwt function added.

--- services/auth-service/src/main/java/com/tkwang312/auth/security/JwtAuthenticationFilter.java
+++ services/auth-service/src/main/java/com/tkwang312/auth/security/JwtAuthenticationFilter.java
@@ -12,7 +12,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 
-// Goes through filter on each client request
+// Goes through filter on each client request.
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
@@ -28,6 +28,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+        // check jwt token
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // "Bearer " is len 7
+        jwt = authHeader.substring(7);
 
     }
 }
